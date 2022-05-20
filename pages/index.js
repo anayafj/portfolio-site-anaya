926e74dcfa9d990e800308e2903e05e0a466c945
anayafj <franciscojanaya80@gmail.com>
2022-05-20 14:38:03 -0700

    fonts added, logo setup need responsive help

--- pages/index.js
+++ pages/index.js
@@ -12,12 +12,35 @@ export default function Home() {
 				<title>Francisco Anaya Portfolio Site</title>
 				<meta name="description" content="Portfolio website using NextJs" />
 				<link rel="icon" href="/favicon.ico" />
+				<link
+					href="http://fonts.cdnfonts.com/css/blank-river"
+					rel="stylesheet"
+				></link>
 			</Head>
 
 			<div className={styles.wrapper}>
-				<Header />
-				<Main />
-				<Section />
+				{/* <Header /> */}
+				{/* <Mavin /> */}
+				{/* <Section /> */}
+				<header className={styles.header}>
+					<div className={styles.logoContainer}>
+						<div className={styles.logoBorder}></div>
+						<div className={styles.logo}>
+							<img src="/svg/logo_name.svg" alt="Logo - Francisco Anaya" />
+						</div>
+						<div className={styles.subheaderContainer}>
+							<h1>Designer</h1>
+							<span></span>
+							<h1>Developer</h1>
+							<span></span>
+							<div className={styles.subheaderEnd}>
+								<h1>Web</h1>
+								<h2>&amp;</h2>
+								<h1>Rich Media</h1>
+							</div>
+						</div>
+					</div>
+				</header>
 			</div>
 		</div>
 	);
