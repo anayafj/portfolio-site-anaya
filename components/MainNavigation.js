78c672b45d2eda2a56960108c50fc5298b5acaf5
anayafj <franciscojanaya80@gmail.com>
2022-07-13 12:00:23 -0700

    navigation ready for routes

--- components/MainNavigation.js
+++ components/MainNavigation.js
@@ -12,9 +12,13 @@ export default function MainNavigation() {
 
 	//gsap
 	const menuContainer = useRef();
+	const pink = useRef();
 	const yellow = useRef();
+	const yellowTxt = useRef();
 	const blue = useRef();
+	const blueTxt = useRef();
 	const white = useRef();
+	const whiteShape = useRef();
 	const closeX = useRef();
 	const menuTxt = useRef();
 	const tl = gsap.timeline({ paused: true });
@@ -78,30 +82,50 @@ export default function MainNavigation() {
 		tl.addLabel('showText', 0.6);
 		tl.addLabel('end', 2);
 		tl.to(closeX.current, { autoAlpha: 0, duration: 0 }, 'start');
-		tl.to(
-			menuContainer.current,
-			{ width: 115, ease: 'expo.InOut', duration: 0.4 },
-			'start',
-		);
-		tl.to(
-			white.current,
-			{ width: 120, ease: 'expo.Out', duration: 0.2 },
-			'start',
-		);
+		tl.to(yellowTxt.current, { autoAlpha: 0, duration: 0 }, 'start');
+		tl.to(blueTxt.current, { autoAlpha: 0, duration: 0 }, 'start');
+		tl.to(pink.current, { width: 0, autoAlpha: 0, duration: 0 }, 'start');
 		tl.to(
 			yellow.current,
-			{ width: 4, x: '-= 74', ease: 'expo.InOut', duration: 0.4 },
-			'start += 0.2',
+			{ width: 4, x: '+=21', ease: 'expo.InOut', duration: 0.4 },
+			'start',
 		);
 		tl.to(
 			blue.current,
-			{ width: 4, x: '-= 74', ease: 'expo.InOut', duration: 0.3 },
-			'start += 0.2',
+			{ width: 4, x: '+=21', ease: 'expo.InOut', duration: 0.2 },
+			'start',
+		);
+		tl.to(
+			whiteShape.current,
+			{
+				borderRightWidth: '320px',
+				duration: 0,
+			},
+			'start',
+		);
+		tl.to(
+			menuContainer.current,
+			{ width: 115, ease: 'expo.Out', duration: 0.6 },
+			'start += 0.1',
 		);
 		tl.to(
 			white.current,
-			{ x: '-= 74', ease: 'expo.InOut', duration: 0.3 },
-			'start += 0.2',
+			{
+				width: 86,
+				x: '+=21',
+				ease: 'expo.Out',
+				duration: 0.5,
+			},
+			'start += 0.1',
+		);
+		tl.to(
+			whiteShape.current,
+			{
+				borderRightWidth: '76px',
+				ease: 'expo.Out',
+				duration: 1,
+			},
+			'start += 0.1',
 		);
 		tl.to(
 			menuTxt.current,
@@ -122,6 +146,7 @@ export default function MainNavigation() {
 		tl.addLabel('start', 0);
 		tl.addLabel('showText', 0.8);
 		tl.addLabel('end', 2);
+
 		tl.to(menuTxt.current, { autoAlpha: 0, duration: 0 }, 'start');
 		tl.to(
 			menuContainer.current,
@@ -130,20 +155,41 @@ export default function MainNavigation() {
 		);
 		tl.to(
 			yellow.current,
-			{ width: 114, x: '+= 74', ease: 'expo.Out', duration: 0.3 },
+			{ width: 114, x: '-= 21', ease: 'expo.Out', duration: 0.3 },
 			'start += 0.2',
 		);
 		tl.to(
 			blue.current,
-			{ width: 114, x: '+= 74', ease: 'expo.Out', duration: 0.3 },
+			{ width: 114, x: '-= 21', ease: 'expo.Out', duration: 0.3 },
 			'start += 0.2',
 		);
 		tl.to(
 			white.current,
-			{ x: '+= 74', ease: 'expo.In', duration: 0.3 },
+			{ x: '-= 21', ease: 'expo.In', duration: 0.3 },
 			'start += 0.2',
 		);
-		tl.to(white.current, { width: 80, duration: 0.3 }, 'showText -= 0.4');
+		tl.to(white.current, { width: 51, duration: 0.3 }, 'showText -= 0.4');
+		tl.to(
+			whiteShape.current,
+			{
+				borderRightWidth: '42px',
+				ease: 'expo.InOut',
+				duration: 0.3,
+			},
+			'showText -= 0.4',
+		);
+
+		tl.to(
+			pink.current,
+			{ width: 106, autoAlpha: 1, duration: 0.2 },
+			'showText -= 0.4',
+		);
+		tl.to(
+			yellowTxt.current,
+			{ autoAlpha: 1, duration: 0.3 },
+			'showText -= 0.4',
+		);
+		tl.to(blueTxt.current, { autoAlpha: 1, duration: 0.4 }, 'showText -= 0.4');
 		tl.to(
 			closeX.current,
 			{
@@ -158,6 +204,23 @@ export default function MainNavigation() {
 		);
 	};
 
+	const navigationHandler = (e) => {
+		// console.log('navigationHandler --- e = ', e.target.id);
+		e.preventDefault();
+
+		switch (e.target.id) {
+			case 'home':
+				console.log('Home tab hit');
+				break;
+			case 'work':
+				console.log('Work tab hit');
+				break;
+			case 'about':
+				console.log('About tab hit');
+				break;
+		}
+	};
+
 	// Styles ----------------------------------------------------------------------->>
 
 	let mainNavContainer = myStyles({ expandedNav: true });
@@ -168,26 +231,48 @@ export default function MainNavigation() {
 		<nav className={styles.navContainer}>
 			<div className={mainNavContainer} ref={menuContainer}>
 				<div className={styles.expNavBg}>
-					<div className={styles.work} ref={yellow}>
+					<div
+						onClick={navigationHandler}
+						id="home"
+						className={styles.home}
+						ref={pink}
+					>
+						<div className={styles.pinkTxt}>
+							<img src="/svg/txt_home.svg" alt="Home tab txt" />
+						</div>
+						<hr></hr>
+					</div>
+					<div
+						onClick={navigationHandler}
+						id="work"
+						className={styles.work}
+						ref={yellow}
+					>
 						<div className={styles.yellow}></div>
-						<div className={styles.yellowTxt}>
+						<div className={styles.yellowTxt} ref={yellowTxt}>
 							<img src="/svg/txt_work.svg" alt="Work tab txt" />
 						</div>
+						<hr></hr>
 					</div>
-					<div className={styles.about} ref={blue}>
+					<div
+						onClick={navigationHandler}
+						id="about"
+						className={styles.about}
+						ref={blue}
+					>
 						<div className={styles.blue}></div>
-						<div className={styles.blueTxt}>
+						<div className={styles.blueTxt} ref={blueTxt}>
 							<img src="/svg/txt_about.svg" alt="About tab txt" />
 						</div>
+						<hr></hr>
 					</div>
 					<div
 						onClick={menuExpColBtnHandler}
 						className={styles.navMenu}
 						ref={white}
 					>
-						<div className={styles.white}></div>
+						<div className={styles.white} ref={whiteShape}></div>
 						<div className={menuExpand} ref={menuTxt}>
-							<img src="/svg/arrow_left.svg" alt="double arrows left" />
 							<img src="/svg/txt_menu.svg" alt="menu text" />
 						</div>
 						<div className={menuCollapse} ref={closeX}>
