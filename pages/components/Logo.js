2f38a9229a0ee822226af2563eca6e4e3263edd0
Francisco Anaya <franciscoanaya@videodrome.local>
2024-04-17 23:09:49 -0700

    Logo on homepage resized

--- pages/components/Logo.js
+++ pages/components/Logo.js
@@ -2,33 +2,18 @@ import { useRouter } from 'next/router';
 import styles from '../../styles/components/Logo.module.scss';
 
 export default function Logo({ version }) {
-	console.log('version = ' + version);
+	// console.log('version = ' + version);
 	const router = useRouter();
 
 	const homeClick = (e) => {
-		console.log('homeClick');
+		// console.log('homeClick');
 		e.preventDefault();
 		router.push('/');
 	};
 
 	return (
-		<div onClick={homeClick} className={styles.logoContainer}>
-			<div className={styles.logo}>
-				<img src="/svg/logo_name.svg" alt="Logo - Francisco Anaya" />
-			</div>
-			<div className={styles.subheaderContainer}>
-				<div className={styles.leftBorder}></div>
-				<div className={styles.subheaderText}>
-					<h1>Designer</h1>
-					<span></span>
-					<h1>Developer</h1>
-					<span></span>
-					<h1>Web</h1>
-					<h2>&amp;</h2>
-					<h1>Rich Media</h1>
-				</div>
-				<div className={styles.rightBorder}></div>
-			</div>
+		<div onClick={homeClick} className={styles.logo}>
+			<img src="/svg/logo_name.svg" alt="Logo - Francisco Anaya" />
 		</div>
 	);
 }
