00da834cf998166c96f75b56e6f20a510a437f83
Francisco Anaya <franciscoanaya@videodrome.local>
2024-10-14 22:03:24 -0700

    about page needs layout help-remove line at bottom right

--- pages/about.js
+++ pages/about.js
@@ -3,9 +3,21 @@ import styles from '../styles/pages/about.module.scss';
 export default function About() {
 
 	return (
-			<div className={styles.aboutPage}>
+		<div className={styles.aboutPage}>
 
-				<div className={styles.top}>
+			<div className={styles.top}>
+				<div className={styles.subheader}>
+					<h1>Work</h1>
+					<h2>Design, Animate, <span>&amp;</span> Code</h2>
+				</div>
+			</div>
+			<div className={styles.mainContainer}>
+				<div className={styles.projectContainer}>
+					{/* <div className={styles.projects}>{renderProjects()}</div> */}
+				</div>
+			</div>
+
+			{/* <div className={styles.top}>
 					<div className={styles.lockup}>
 						<div className={styles.photo}>
 							<img src="/images/about_photo.png" alt="Photo" />
@@ -14,8 +26,8 @@ export default function About() {
 							<img src="/svg/logo_name_sm.svg" alt="Francisco Anaya" />
 						</div>
 					</div>
-				</div>
-				<div className={styles.bottom}>
+				</div> */}
+			{/* <div className={styles.bottom}>
 					<div className={styles.subheader}>
 						<h1>
 							I design, animate, <span>&amp;</span> code. Bringing a mixture of
@@ -26,8 +38,8 @@ export default function About() {
 						<img src="/svg/homeNav_me.svg" alt="About Me Logo" />
 						<div className={styles.lines}></div>
 					</div>
-				</div>
-				<hr noshade="false" />
-			</div>
+				</div> */}
+			<hr noshade="false" />
+		</div>
 	);
 }
