2cad200226c13aa869b479828365e47f71a83762
Francisco Anaya <anayafj@gmail.com>
2024-10-16 12:51:54 -0700

    adding text to about page - img set

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,5 @@
 import styles from '../styles/pages/about.module.scss';
+import Image from 'next/image';
 
 export default function About() {
 
@@ -12,36 +13,33 @@ export default function About() {
 				</div>
 			</div>
 			<div className={styles.mainContainer}>
+				<div className={styles.infoContainer}>
+					<div className={styles.infoHeader}>
+						<h1>Hi, I’m frankie</h1>
+					</div>
+					<div className={styles.infoBody}>
+						<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent imperdiet purus vitae lectus pellentesque, vel rhoncus lorem sagittis. Integer at risus nec felis sodales faucibus quis eu ante. Quisque at mattis dolor. Morbi et sem mattis, sollicitudin velit vitae, porta orci. Donec ut tortor venenatis, faucibus ex vitae, tincidunt urna. Ut pulvinar pellentesque eros eu tempus. Mauris finibus velit quis ante dictum pharetra. Mauris mollis nisi sed nulla imperdiet, sed varius mi fringilla. Nullam venenatis rutrum cursus. Integer consectetur urna eu quam sagittis, vitae semper sapien lobortis. Duis quis nibh sed ligula luctus blandit. Nullam facilisis mi nec velit tristique viverra. Nullam ante elit, suscipit id ante et, eleifend suscipit lorem.
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent imperdiet purus vitae lectus pellentesque, vel rhoncus lorem sagittis. Integer at risus nec felis sodales faucibus quis eu ante. Quisque at mattis dolor. Morbi et sem mattis, sollicitudin velit vitae, porta orci. Donec ut tortor venenatis, faucibus ex vitae, tincidunt urna. Ut pulvinar pellentesque eros eu tempus.ndit. Nullam facilisis mi nec velit tristique viverra. Nullam ante elit, suscipit id ante et, eleifend suscipit lorem.</h2>
+					</div>
+					
+				</div>
 				<div className={styles.backgroundImage}>
-					<img src="/images/nopal_large.png" alt="Close-up of a nopal" />
+					<Image
+						src="/images/nopal.png"
+						alt="Close-up of a nopal"
+						width={694}
+						height={770}
+						style={{
+							width: 'auto',
+							height: 'auto',
+						  }}
+					/>
 				</div>
-				<div className={styles.projectContainer}>
-					{/* <div className={styles.projects}>{renderProjects()}</div> */}
-				</div>
+				
+
 			</div>
 
-			{/* <div className={styles.top}>
-					<div className={styles.lockup}>
-						<div className={styles.photo}>
-							<img src="/images/about_photo.png" alt="Photo" />
-						</div>
-						<div className={styles.name}>
-							<img src="/svg/logo_name_sm.svg" alt="Francisco Anaya" />
-						</div>
-					</div>
-				</div> */}
-			{/* <div className={styles.bottom}>
-					<div className={styles.subheader}>
-						<h1>
-							I design, animate, <span>&amp;</span> code. Bringing a mixture of
-							diverse skills and experieces to all projects.
-						</h1>
-					</div>
-					<div className={styles.tabLogo}>
-						<img src="/svg/homeNav_me.svg" alt="About Me Logo" />
-						<div className={styles.lines}></div>
-					</div>
-				</div> */}
+			
 			<hr noshade="false" />
 		</div>
 	);
