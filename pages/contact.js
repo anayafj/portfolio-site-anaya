93c33b52d87e739b89711415174f5b7decfcce94
Francisco Anaya <franciscoanaya@videodrome.local>
2024-12-29 23:10:09 -0800

    contact page layout set: make responsive and add form

--- pages/contact.js
+++ pages/contact.js
@@ -14,37 +14,36 @@ export default function Contact() {
 			</div>
 			<div className={styles.mainContainer}>
 				<div className={styles.headerContainer}>
-				<Image
+					<Image
 						src="/svg/contactMe_txt.svg"
 						alt="CONTACT ME - Header text"
 						width={1403}
 						height={115}
-						style={{
-							width: '103%',
-							height: 'auto',
-						  }}
+						// style={{
+						// 	width: '103%',
+						// 	height: 'auto',
+						//   }}
 					/>				
-					{/* <div className={styles.infoHeader}>
-							<h1>CONTACT ME</h1> */}
-							{/* <h2>"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."</h2> */}
-					{/* </div> */}
-					{/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent imperdiet purus vitae lectus pellentesque, vel rhoncus lorem sagittis. Integer at risus nec felis sodales faucibus quis eu ante. Quisque at mattis dolor. Morbi et sem mattis, sollicitudin velit vitae, porta orci. Donec ut tortor venenatis, faucibus ex vitae, tincidunt urna. Ut pulvinar pellentesque eros eu tempus. Mauris finibus velit quis ante dictum pharetra. Mauris mollis nisi sed nulla imperdiet, sed varius mi fringilla. Nullam venenatis rutrum cursus. Integer consectetur urna eu quam sagittis, vitae semper sapien lobortis. Duis quis nibh sed ligula luctus blandit. Nullam facilisis mi nec velit tristique viverra. Nullam ante elit, suscipit id ante et, eleifend suscipit lorem.
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent imperdiet purus vitae lectus pellentesque, vel rhoncus lorem sagittis. Integer at risus nec felis sodales faucibus quis eu ante. Quisque at mattis dolor. Morbi et sem mattis, sollicitudin velit vitae, porta orci. Donec ut tortor venenatis, faucibus ex vitae, tincidunt urna. Ut pulvinar pellentesque eros eu tempus.ndit. Nullam facilisis mi nec velit tristique viverra. Nullam ante elit, suscipit id ante et, eleifend suscipit lorem.</p> */}
 				</div>
-				<div className={styles.pigeonImage}>
-					<Image
-						src="/images/pigeonMessenger.png"
-						alt="Cyborg Messenger Pigeon"
-						width={2048}
-						height={2048}
-						style={{
-							width: 'auto',
-							height: 'auto',
-						  }}
-					/>
-				</div>
-				<div className={styles.contactFormContainer}>
-
+					
+				<div className={styles.contentContainer}>
+				
+					<div className={styles.pigeonImage}>
+						<Image
+							src="/images/pigeonMessenger.png"
+							alt="Cyborg Messenger Pigeon"
+							width={2048}
+							height={2048}
+							
+						/>
+					</div>
+					<div className={styles.formContainer}>
+						<div className={styles.subheader}>
+							<p>Fill out this form and drop me a messege.</p>
+						</div>
+						<div className={styles.contactFormContainer}
+							></div>
+					</div>
 				</div>
 			</div>
 			
