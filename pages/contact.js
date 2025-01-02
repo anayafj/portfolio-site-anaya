009ef7f13e70f1235d44b628ce7e26f5fab40e01
Francisco Anaya <franciscoanaya@videodrome.local>
2025-01-02 11:25:46 -0800

    contact page - ready to add form elements

--- pages/contact.js
+++ pages/contact.js
@@ -5,13 +5,6 @@ export default function Contact() {
 
 	return (
 		<div className={styles.contactPage}>
-
-			<div className={styles.top}>
-				{/* <div className={styles.subheader}>
-					<h1> About Me</h1>
-					<h2>Let me introduce myselfe</h2>
-				</div> */}
-			</div>
 			<div className={styles.mainContainer}>
 				<div className={styles.headerContainer}>
 					<Image
@@ -19,30 +12,22 @@ export default function Contact() {
 						alt="CONTACT ME - Header text"
 						width={1403}
 						height={115}
-						// style={{
-						// 	width: '103%',
-						// 	height: 'auto',
-						//   }}
 					/>				
-				</div>
-					
+				</div>					
 				<div className={styles.contentContainer}>
-				
 					<div className={styles.pigeonImage}>
 						<Image
 							src="/images/pigeonMessenger.png"
 							alt="Cyborg Messenger Pigeon"
 							width={2048}
 							height={2048}
-							
 						/>
 					</div>
 					<div className={styles.formContainer}>
 						<div className={styles.subheader}>
 							<p>Fill out this form and drop me a messege.</p>
 						</div>
-						<div className={styles.contactFormContainer}
-							></div>
+						<div className={styles.contactFormContainer}></div>
 					</div>
 				</div>
 			</div>
