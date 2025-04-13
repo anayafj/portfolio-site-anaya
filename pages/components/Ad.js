72c535a61cbab42f8f8ac6c2e0f4a4f19d560ab7
Francisco Anaya <franciscoanaya@videodrome.local>
2025-04-12 23:14:48 -0700

    connecting work source with thumbnails

--- pages/components/Ad.js
+++ pages/components/Ad.js
@@ -1,15 +1,30 @@
-const pathToAds = '/work/ads/';
-// public/work/ads/banner_amazon_transparent/az_transparent.mp4
-// /Users/franciscoanaya/Documents/WebDevelopment/PersonalWork/portfolio-site-anaya-2023/website-for-me/public/work/ads/banner_amazon_transparent/az_transparent.mp4
+// const pathToAds = '/work/ads/';
+// const pathToMotion = "/work/motion/";
 
 export default function Ad(file) {
-	const mediaObj = file.dataFile;
-	const filePath = mediaObj.fileName;
-	console.log("Data file = ",mediaObj);
-	console.log("filePath = ",filePath);
-	
-	// const dataSrc = pathToAds + 'banner_hbo_watchmen/300x250/index.html';
-	const dataSrc = pathToAds + filePath;
+	// const filePath = file.dataFile.fileName;
+	const mediaType = file.dataFile.mediaType;
+	// console.log("Data file = ",mediaObj);
+
+	const mainPath = "/work/"+mediaType+"/"+file.dataFile.fileName;
+	const dataSrc = mainPath;
+
+	// const dataSrc = (mediaType) => {
+	// 	let src = null;
+	// 	switch(mediaType){
+	// 		case motion: 
+	// 			src = pathToMotion +filePath;
+	// 		break;
+	// 		case banner:
+	// 			src = pathToAds + filePath
+	// 		break;
+	// 	}		
+	// 	console.log("src = ",src);
+	// 	return src;
+	// };
+
+	console.log("dataSrc = ",dataSrc);
+
 	return (
 		<iframe src={dataSrc} width={300} height={250}></iframe>
 	);
